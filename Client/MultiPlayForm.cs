70c2dd1ca5b9923ec3a1d4541436a3d787a7124f
sanaandmomo <qajjang46@naver.com>
2021-09-12 23:51:12 +0900

    1.[버그픽스] 재시작시 roomID 이상하게 보내서 재시작 안된 버그

--- Client/MultiPlayForm.cs
+++ Client/MultiPlayForm.cs
@@ -112,8 +112,7 @@ namespace Client
             paintCoord(x, y, nowPlayer);
 
             // 놓은 바둑돌의 위치 보내기
-            string message = label("Put") + roomID + "," + x + "," + y;
-            SendMessage(message);
+            SendMessage(label("Put") + roomID + "," + x + "," + y);
 
             if (judge(nowPlayer))
             {
@@ -263,7 +262,7 @@ namespace Client
             playing = true;
 
             // 서버로 메세지 보내기
-            SendMessage(label("Play") + roomID);
+            SendMessage(label("Play") + roomID + "]");
 
             this.status.Text = "상대 플레이어의 준비를 기다립니다.";
             this.playButton.Enabled = false;
